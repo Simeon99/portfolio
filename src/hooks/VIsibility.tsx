02bafe3921470af8502cc69726502d4dbc593310
Simeon99 <simeonilic@yahoo.com>
2024-06-01 01:32:46 +0200

    Fix animations

--- src/hooks/VIsibility.tsx
+++ src/hooks/VIsibility.tsx
@@ -3,19 +3,21 @@
 import { useEffect, useState } from "react";
 
 export function useIsVisible(ref: any) {
-    const [isIntersecting, setIntersecting] = useState(false);
-  
-    useEffect(() => {
-      const observer = new IntersectionObserver(([entry]) => {
-          setIntersecting(entry.isIntersecting)
-      } 
-      );
-      
-      observer.observe(ref.current);
-      return () => {
-        observer.disconnect();
-      };
-    }, [ref]);
-  
-    return isIntersecting;
-  }
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting);
+    });
+
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
